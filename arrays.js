0cd2b8ede487f8afb779d81ee754e03c3db37fbe
Amber Shipley <ashipley2@me.com>
2022-11-09 12:13:29 -0800

    Add notes about index positioning

--- arrays.js
+++ arrays.js
@@ -13,3 +13,6 @@ console.log(ages)
 //This line of code calls the push method on the likesCats Array. The push element will add one or more elements to the end of an Array and returns the new length of said Array.
 likesCats.push(false)
 console.log(likesCats)
+
+//Index positions tell you which position an element is in in the Array. It counts up from 0.
+//For example, in the friends Array, Nate would be in position 1, and Michelle would be in position 3.
